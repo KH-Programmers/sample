d2338f0eced5bb4a8b8275a6d2c368cc202a66eb
현중 <yeonhj0507@naver.com>
2023-03-19 00:27:30 +0900

    foodPage modified

--- sample/foodApp/views.py
+++ sample/foodApp/views.py
@@ -12,12 +12,14 @@ def load(request) :
 
 def foodGet() :
     date = datetime.today().strftime("%Y%m%d") 
-    url = "https://open.neis.go.kr/hub/mealServiceDietInfo?key=16c6ed183ccf402dbff0d174ff8a04d7&Type=jsonp&Index=1&pSize=100&ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7010126&MLSV_YMD="+"20230320"
+    url = "https://open.neis.go.kr/hub/mealServiceDietInfo?key=16c6ed183ccf402dbff0d174ff8a04d7&Type=jsonp&Index=1&pSize=100&ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7010126&MLSV_YMD="+date
     response = requests.get(url)
     contents = response.text
     print(contents)
     json_ob = json.loads(contents)
-    body = json_ob['mealServiceDietInfo'][1]['row'][0]['DDISH_NM']
-    
+    if list(json_ob.keys())[0] == "RESULT":
+        body = "오늘 급식이 없습니다."
+    else:
+        body = json_ob['mealServiceDietInfo'][1]['row'][0]['DDISH_NM']
     return body
 
