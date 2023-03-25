9066fcadc77a4b4e8530ce0f4d70e33b4a1ddf9f
dawnfire05 <dawnfire05@naver.com>
2023-03-25 11:23:51 +0900

    login 기능 수정중

--- sample/sample/settings.py
+++ sample/sample/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'contentApp.apps.ContentappConfig',
+    'userApp.apps.UserappConfig'
 ]
 
 MIDDLEWARE = [
--- sample/userApp/models.py
+++ sample/userApp/models.py
@@ -1,2 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from datetime import datetime
 
+
+
+class User(models.Model):
+    user_id = models.CharField(max_length=20)
+    user_name = models.CharField(max_length=10)
+    user_creation_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user_id
