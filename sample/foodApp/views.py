d3cf5305bb57046aad14fd3e2ca10522b50e9e85
현중 <yeonhj0507@naver.com>
2023-03-25 10:39:46 +0900

    self merge

--- sample/foodApp/views.py
+++ sample/foodApp/views.py
@@ -8,16 +8,19 @@ import requests
 # Create your views here.
 def load(request) :
     content = foodGet()
-    return views.main(request, content)
+    title = "food"
+    return views.main(request, content, title)
 
 def foodGet() :
     date = datetime.today().strftime("%Y%m%d") 
-    url = "https://open.neis.go.kr/hub/mealServiceDietInfo?key=16c6ed183ccf402dbff0d174ff8a04d7&Type=jsonp&Index=1&pSize=100&ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7010126&MLSV_YMD="+"20230320"
+    url = "https://open.neis.go.kr/hub/mealServiceDietInfo?key=16c6ed183ccf402dbff0d174ff8a04d7&Type=jsonp&Index=1&pSize=100&ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7010126&MLSV_YMD="+date
     response = requests.get(url)
     contents = response.text
     print(contents)
     json_ob = json.loads(contents)
-    body = json_ob['mealServiceDietInfo'][1]['row'][0]['DDISH_NM']
-    
+    if list(json_ob.keys())[0] == "RESULT":
+        body = "오늘 급식이 없습니다."
+    else:
+        body = json_ob['mealServiceDietInfo'][1]['row'][0]['DDISH_NM']
     return body
 
