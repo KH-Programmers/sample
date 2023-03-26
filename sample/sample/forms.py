4459d0816b78522887474b9232d77558296ff0da
dawnfire05 <dawnfire05@naver.com>
2023-03-26 23:03:09 +0900

    userPage 각종 버그 수정 및 회원가입 기능 추가

--- sample/sample/forms.py
+++ sample/sample/forms.py
@@ -2,8 +2,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-class UseForm(UserCreationForm):
-    email = fomrs.EmailField(label="이메일")
+class UserForm(UserCreationForm):
+    email = forms.EmailField(label="이메일")
 
     class Meta:
         model = User
