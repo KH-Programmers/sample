b53be09dfc61646e1a478706ff72ff9a0f9503eb
현중 <yeonhj0507@naver.com>
2023-03-26 22:59:15 +0900

    3/26 10:39 충돌 수정

--- sample/sample/forms.py
+++ sample/sample/forms.py
@@ -3,11 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class UseForm(UserCreationForm):
-<<<<<<< HEAD
     email = forms.EmailField(label="이메일")
-=======
-    email = fomrs.EmailField(label="이메일")
->>>>>>> 30b9d28800fdd86d54464482e6df5fefbae708de
 
     class Meta:
         model = User
--- sample/userApp/urls.py
+++ sample/userApp/urls.py
@@ -2,17 +2,10 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 
-<<<<<<< HEAD
-app_name = "userApp"
-
-urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login')
-=======
 app_name = 'userApp'
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
     path('signup/', views.signup, name="signup")
->>>>>>> 30b9d28800fdd86d54464482e6df5fefbae708de
 ]
