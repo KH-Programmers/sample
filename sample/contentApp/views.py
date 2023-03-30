9cb4190c999c211f5c409a7d202cf18294a145b7
현중 <65106888+yeonhj0507@users.noreply.github.com>
2023-03-31 07:44:20 +0900

    Revert "DEFY_AK15"

--- sample/contentApp/views.py
+++ sample/contentApp/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404
 from .models import contentModel
 from mainApp import views
-import datetime
+
 # Create your views here.
 def indexView(request):
-    content_list = contentModel.objects.all().order_by("-id")
+    content_list = contentModel.objects.all()
     var = { 'content_list' : content_list }
     title = 'contents/index'
     return views.main(request, var ,title)
@@ -13,19 +13,11 @@ def contentView(request, content_id):
     content = get_object_or_404(contentModel,pk=content_id)
     var = {
         'Title': content.Title,
-        'text' : content.text,
-        'date' : content.date,
+        'text' : content.text
         }
     title = 'contents/content'
     return views.main(request, var ,title)
 
 def contentWrite(request):
     title = 'contents/write'
-    if request.method == "POST":
-        model = contentModel()
-        model.date = datetime.datetime.now()
-        model.text = request.POST["text"]
-        model.Title = request.POST["title"]
-        model.save()
-        return redirect("contentApp:index")
     return views.main(request, " ", title)
