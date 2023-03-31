6a6283d20716331f5964a255a112a9aecdde0192
DEFY-AK15 <kjy291002@gmail.com>
2023-03-31 17:00:19 +0900

    이거날라가면접음

--- sample/contentApp/views.py
+++ sample/contentApp/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import contentModel
 from mainApp import views
+import datetime
 
 # Create your views here.
 def indexView(request):
-    content_list = contentModel.objects.all()
+    content_list = contentModel.objects.all().order_by("-id")
     var = { 'content_list' : content_list }
     title = 'contents/index'
     return views.main(request, var ,title)
@@ -13,11 +14,19 @@ def contentView(request, content_id):
     content = get_object_or_404(contentModel,pk=content_id)
     var = {
         'Title': content.Title,
-        'text' : content.text
+        'text' : content.text,
+        'date' : content.date,
         }
     title = 'contents/content'
     return views.main(request, var ,title)
 
 def contentWrite(request):
     title = 'contents/write'
+    if request.method == "POST":
+        model = contentModel()
+        model.date = datetime.datetime.now()
+        model.text = request.POST["text"]
+        model.Title = request.POST["title"]
+        model.save()
+        return redirect("contentApp:index")
     return views.main(request, " ", title)
