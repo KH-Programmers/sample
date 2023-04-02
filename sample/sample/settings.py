8ff59a3978b7bf083b968d2cc88ea205f483c240
lto2332 <lto05@naver.com>
2023-04-03 05:36:21 +0900

    프로필창

--- sample/sample/settings.py
+++ sample/sample/settings.py
@@ -119,14 +119,17 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-#커밋좀 하자 좀
 LOGOUT_REDIRECT_URL = "/"
